b58bb91d90cb36180713e502a637debf85558c12
kyliefromm <90372645+kyliefromm@users.noreply.github.com>
2022-02-16 08:34:57 -0700

    made mission 7
    made mission 7

---
+++ Mission7/Infrastructure/PaginationTagHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Mission7.Models.ViewModels;
+
+namespace Mission7.Infrastructure
+{
+    [HtmlTargetElement("div", Attributes = "page-blah")]
+    public class PaginationTagHelper : TagHelper
+    {
+        private IUrlHelperFactory uhf;
+
+        public PaginationTagHelper(IUrlHelperFactory temp)
+        {
+            uhf = temp;
+        }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext vc { get; set; }
+
+        public PageInfo PageBlah { get; set; }
+      
+
+        public string PageAction { get; set; }
+
+
+
+        public override void Process(TagHelperContext thc, TagHelperOutput tho)
+        {
+            IUrlHelper uh = uhf.GetUrlHelper(vc);
+
+            TagBuilder final = new TagBuilder("div");
+
+            for (int i = 1; i <= PageBlah.TotalPages; i++)
+            {
+                TagBuilder tb = new TagBuilder("a");
+
+                tb.Attributes["href"] = uh.Action(PageAction, new { pageNum = i });
+                tb.InnerHtml.Append(i.ToString());
+
+                final.InnerHtml.AppendHtml(tb);
+            }
+            tho.Content.AppendHtml(final.InnerHtml);
+        }
+
+
+
+    
+        }
+    }
